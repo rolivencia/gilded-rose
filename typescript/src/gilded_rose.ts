4c1d2f9d52725b2d2c1cc5445efc890a06440572
rolivencia <ramiro.olivencia@latch.com>
2025-06-05 19:28:51 -0300

    Added refactoring plus implementation for Milk handling

--- typescript/src/gilded_rose.ts
+++ typescript/src/gilded_rose.ts
@@ -7,51 +7,75 @@ class GildedRose {
     }
   }
 
-  processItemEndOfDay(item: Item): void {
-    if (item.name != 'Aged Cheddar' && item.name != 'Concert Tickets') {
-      if (item.quality > 0) {
-        if (item.name != 'Hammer') {
-          item.quality = item.quality - 1;
-        }
+  private processAgedCheddarEndOfDay(item: Item): void {
+    const MAX_QUALITY_THRESHOLD = 50;
+    item.daysRemaining = item.daysRemaining - 1;
+    item.quality += item.daysRemaining < 0 ? 2 : 1;
+    item.quality = Math.min(item.quality, MAX_QUALITY_THRESHOLD);
+  }
+
+  private processConcertTicketsEndOfDay(item: Item): void {
+    const MAX_QUALITY_THRESHOLD = 50;
+    const DAYS_UPPER_BOUND = 10;
+    const DAYS_LOWER_BOUND = 5;
+    item.daysRemaining -= 1;
+
+    item.quality = [
+      DAYS_UPPER_BOUND,
+      DAYS_LOWER_BOUND
+    ].reduce((accum, bound) => {
+      if(item.daysRemaining < bound) {
+        accum++;
       }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == 'Concert Tickets') {
-          if (item.daysRemaining < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.daysRemaining < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (item.name != 'Hammer') {
-      item.daysRemaining = item.daysRemaining - 1;
-    }
+      return accum;
+    }, item.quality + 1)
+
     if (item.daysRemaining < 0) {
-      if (item.name != 'Aged Cheddar') {
-        if (item.name != 'Concert Tickets') {
-          if (item.quality > 0) {
-            if (item.name != 'Hammer') {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+      item.quality = 0;
+    }
+
+    item.quality = Math.min(item.quality, MAX_QUALITY_THRESHOLD);
+  }
+
+  private processRandomItemEndOfDay(item: Item): void {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+
+    item.daysRemaining = item.daysRemaining - 1;
+
+    if (item.daysRemaining < 0) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
       }
     }
   }
+
+  private processMilkEndOfDay(item: Item): void {
+    const MIN_QUALITY_THRESHOLD = 0;
+    item.daysRemaining -= 1;
+    item.quality -= item.daysRemaining > 0 ? 2 : 4
+    item.quality = Math.max(MIN_QUALITY_THRESHOLD, item.quality);
+  }
+
+  processItemEndOfDay(item: Item): void {
+    switch(item.name) {
+      case 'Aged Cheddar':
+        this.processAgedCheddarEndOfDay(item);
+        return;
+      case 'Concert Tickets':
+        this.processConcertTicketsEndOfDay(item);
+        return;
+      case 'Hammer':
+        return;
+      case 'Milk':
+        this.processMilkEndOfDay(item);
+        return;
+      default:
+        this.processRandomItemEndOfDay(item);
+      return;
+      }
+    }
 }
 
 export { GildedRose };
